1fef777aa31431ff727ccfa577ed8526c8b4e9a8
Adesh Landge <37284267+adeshlandge@users.noreply.github.com>
2022-12-11 22:19:11 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME', 'adeshlan');
-define('DB_PASSWORD', 'Ilovesman1998@a');
-define('DB_NAME', 'adeshlan_WPZJS');
+define('DB_USERNAME', 'yourdbusername');
+define('DB_PASSWORD', 'yourdbpassword');
+define('DB_NAME', 'yourdbname');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
